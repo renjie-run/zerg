738bdd15954e0a0fba5d1f0894b027efd9109eb0
renjie-run <rj.aiyayao@gmail.com>
2023-04-30 23:16:25 +0800

    common: curl_get

--- app/common.php
+++ app/common.php
@@ -1,2 +1,22 @@
 <?php
 // 应用公共文件
+
+/**
+ * @param string $url get 请求地址
+ * @param init $httpCode 返回状态码
+ * @return mixed
+ */
+function curl_get($url, &$httpCode = 0)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    // 不做证书校验，linux 环境下请改为 true
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+    $file_contents = curl_exec($ch);
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+    return $file_contents;
+}
