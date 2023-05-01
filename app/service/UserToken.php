918be0a726c3ee125d006932d39335fc746c6b8b
renjie-run <rj.aiyayao@gmail.com>
2023-05-01 15:20:39 +0800

    generate & cache token

--- app/service/UserToken.php
+++ app/service/UserToken.php
@@ -2,11 +2,12 @@
 
 namespace app\service;
 
+use app\exception\TokenException;
 use app\exception\WechatException;
 use think\Exception;
 use app\model\User as UserModel;
 
-class UserToken
+class UserToken extends Token
 {
     protected $wxJsCode;
     protected $wxAppId;
@@ -32,28 +33,50 @@ class UserToken
         if (array_key_exists('errcode', $wxResult)) {
             $this->processWxException($wxResult);
         }
-        $uid = $this->grantToken($wxResult);
-        return 'token';
+        return $this->grantToken($wxResult);
     }
 
     private function grantToken($wxResult)
     {
         // 获得 openid
-        $opendid = $wxResult['openid'];
+        $openid = $wxResult['openid'];
 
         // 根据 openid 查看 User 表中是否存在对应的记录
         // 如果存在则不处理，不存在则需要在 User 表中插入一条新的记录
-        $user = (new UserModel())->getUserByOpenid($opendid);
-        if (!$user) {
-            $uid = $user->id;
-        } else {
-            $user = $this->newUser($opendid);
-            $uid = $user->id;
+        $user = (new UserModel())->getUserByOpenid($openid);
+        if (!$user || $user->isEmpty()) {
+            $user = $this->newUser($openid);
         }
+        $uid = $user->id;
 
-        // 生成令牌
+        // 生成令牌: { key: token, value: [ $wxResult, uid, scope] }
         // 准备缓存数据，写入缓存
         // 将令牌返回给客户端
+        $cacheValue = $this->prepareCacheValue($wxResult, $uid);
+        $token = $this->saveToCache($cacheValue);
+        return $token;
+    }
+
+    private function saveToCache($cacheValue)
+    {
+        $name = $this->generateToken();
+        $value = json_encode($cacheValue);
+        $request = cache($name, $value, config('setting.token_expire_in'));
+        if (!$request) {
+            throw new TokenException([
+                'msg' => '服务器缓存异常',
+                'err_code' => 10005,
+            ]);
+        }
+        return $name;
+    }
+
+    private function prepareCacheValue($wxResult, $uid)
+    {
+        $cacheValue = $wxResult;
+        $cacheValue['uid'] = $uid;
+        $cacheValue['scope'] = 16;
+        return $cacheValue;
     }
 
     private function newUser($openid)
--- config/setting.php
+++ config/setting.php
@@ -3,4 +3,5 @@
 return [
     'img_prefix' => 'http://zerg.cn/images',
     'file_prefix' => 'http://zerg.cn/files',
+    'token_expire_in' => 7200,
 ];
